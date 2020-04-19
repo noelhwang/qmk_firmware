88e83ebf1a8175e6ee2d336d012b380ac6004e54
noelhwang <noelhwang@gmail.com>
2020-04-18 20:06:10 -0400

    Test Keymap for Meira

--- keyboards/meira/featherble/rules.mk
+++ keyboards/meira/featherble/rules.mk
@@ -3,3 +3,12 @@ F_CPU = 8000000
 
 BLUETOOTH_ENABLE = yes
 BLUETOOTH = AdafruitBLE
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = caterina
